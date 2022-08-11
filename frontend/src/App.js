acb3dcaa2bf233cead7e6b755335701720ac78dd
Afsul Rahman <afsulult@gmail.com>
2022-08-11 17:49:45 +0530

    basic crud

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddMovie from './ AddMovie';
+import './App.css';
+
+
+function App() {
+   
+  return (
+    <div className="App">
+      <AddMovie/>
+      
+    </div>
+  );
+}
+
+export default App;
